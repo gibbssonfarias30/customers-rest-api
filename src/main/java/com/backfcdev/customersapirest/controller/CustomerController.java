997732219a5257ac4a429820f8b469f215163dda
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-02 15:16:50 -0500

    added modelmapper to controller

--- src/main/java/com/backfcdev/customersapirest/controller/CustomerController.java
+++ src/main/java/com/backfcdev/customersapirest/controller/CustomerController.java
@@ -4,21 +4,24 @@ import com.backfcdev.customersapirest.model.Customer;
 import com.backfcdev.customersapirest.model.dto.CustomerDTO;
 import com.backfcdev.customersapirest.service.ICustomerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
 
     private final ICustomerService customerService;
+    private final ModelMapper mapper;
 
     @GetMapping
-    ResponseEntity<List<Customer>> findAllCustomers(){
-        return new ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
+    ResponseEntity<List<Customer>> findAll(){
+        return ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
@@ -39,6 +42,15 @@ public class CustomerController {
     @DeleteMapping("/{id}")
     ResponseEntity<Long> deleteCustomerById(@PathVariable Long id){
         return new ResponseEntity<>(customerService.deleteById(id), HttpStatus.NO_CONTENT);
+    }
+
+
+    public Customer convertToEntity(CustomerDTO dto){
+        return mapper.map(dto, Customer.class);
+    }
+
+    public CustomerDTO convertToDto(Customer entity){
+        return mapper.map(entity, CustomerDTO.class);
     }
 
 }
