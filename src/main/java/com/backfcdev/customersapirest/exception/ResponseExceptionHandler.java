124f89a9840486df8d8716bb23b5e517bdd51979
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-02 15:41:08 -0500

    changed exception, use of ProblemDetail in spring boot 3

--- src/main/java/com/backfcdev/customersapirest/exception/ResponseExceptionHandler.java
+++ src/main/java/com/backfcdev/customersapirest/exception/ResponseExceptionHandler.java
@@ -1,22 +1,24 @@
 package com.backfcdev.customersapirest.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class CustomerNotFoundAdvice {
+public class ResponseExceptionHandler {
     @ResponseBody
     @ExceptionHandler(CustomerNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> exceptionHandler(CustomerNotFoundException exception){
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", exception.getMessage());
-        return errorMap;
+    ProblemDetail handleCustomerNotFoundException(CustomerNotFoundException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Customer Not Found");
+        problemDetail.setType(URI.create("/not-found"));
+        return problemDetail;
     }
 }
