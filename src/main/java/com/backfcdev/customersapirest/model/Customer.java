e6e3d8bd7592d894760c189a1e247913ee4e4ee3
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-02 14:53:04 -0500

    removing imports

--- src/main/java/com/backfcdev/customersapirest/model/Customer.java
+++ src/main/java/com/backfcdev/customersapirest/model/Customer.java
@@ -1,9 +1,7 @@
 package com.backfcdev.customersapirest.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
