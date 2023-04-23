df3e33ca2aca44ee11c56910ab6952fac844aa93
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-23 08:58:09 -0500

    the class was extended from ResponseEntityExceptionHandler, also added the date the exception occurs, and removed redundant imports

--- src/main/java/com/backfcdev/customersapirest/exception/ResponseExceptionHandler.java
+++ src/main/java/com/backfcdev/customersapirest/exception/ResponseExceptionHandler.java
@@ -3,22 +3,21 @@ package com.backfcdev.customersapirest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Instant;
 
 @RestControllerAdvice
-public class ResponseExceptionHandler {
-    @ResponseBody
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(CustomerNotFoundException.class)
     ProblemDetail handleCustomerNotFoundException(CustomerNotFoundException ex){
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         problemDetail.setTitle("Customer Not Found");
         problemDetail.setType(URI.create("/not-found"));
+        problemDetail.setProperty("timestamp", Instant.now());
         return problemDetail;
     }
 }
