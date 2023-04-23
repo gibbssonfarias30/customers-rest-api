0093b215e8962823bf46b35808806c5abe05f7c9
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-23 13:20:43 -0500

    added the logic in the handleMethodArgumentNotValidException exception

--- src/main/java/com/backfcdev/customersapirest/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/backfcdev/customersapirest/exception/handler/GlobalExceptionHandler.java
@@ -1,17 +1,51 @@
 package com.backfcdev.customersapirest.exception.handler;
 
 import com.backfcdev.customersapirest.exception.CustomerNotFoundException;
+import com.backfcdev.customersapirest.exception.ValidationError;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler  {
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String, List<ValidationError>> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(FieldError::getField, Collectors.mapping(
+                        fieldError -> new ValidationError(
+                                fieldError.getCode(),
+                                messageSource.getMessage(fieldError, Locale.getDefault())),
+                        Collectors.toList()
+                )));
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, null);
+        problemDetail.setTitle("Validation error");
+        problemDetail.setDetail("Validation errors found");
+        problemDetail.setType(URI.create("/bad-request"));
+        problemDetail.setProperty("errors", errors);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
 
     @ExceptionHandler(CustomerNotFoundException.class)
     ProblemDetail handleCustomerNotFoundException(CustomerNotFoundException ex){
