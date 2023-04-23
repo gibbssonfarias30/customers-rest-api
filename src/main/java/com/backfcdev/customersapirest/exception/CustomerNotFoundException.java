fa0a2bb4b83036f37158a1164bd803c3e86ada0f
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-23 08:51:41 -0500

    removed CustomerNotFoundException constructor in class the exception

--- src/main/java/com/backfcdev/customersapirest/exception/CustomerNotFoundException.java
+++ src/main/java/com/backfcdev/customersapirest/exception/CustomerNotFoundException.java
@@ -1,7 +1,4 @@
 package com.backfcdev.customersapirest.exception;
 
 public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(Long id){
-        super("Could not found the customer with id " + id);
-    }
 }
