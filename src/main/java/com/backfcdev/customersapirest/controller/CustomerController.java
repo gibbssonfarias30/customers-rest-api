53ae9cc3ec63d41f32b115663429aed09fb29bb7
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-23 13:27:46 -0500

    added clean code, @Validated annotation, and in the post used ServletUriComponentsBuilder to build a URI for a new resource created in the application.

--- src/main/java/com/backfcdev/customersapirest/controller/CustomerController.java
+++ src/main/java/com/backfcdev/customersapirest/controller/CustomerController.java
@@ -4,17 +4,20 @@ import com.backfcdev.customersapirest.model.Customer;
 import com.backfcdev.customersapirest.model.dto.CustomerDTO;
 import com.backfcdev.customersapirest.service.ICustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
-
     private final ICustomerService customerService;
     private final ModelMapper mapper;
 
@@ -27,10 +30,12 @@ public class CustomerController {
     }
 
     @PostMapping
-    ResponseEntity<CustomerDTO> save(@RequestBody CustomerDTO dto){
+    ResponseEntity<Void> save(@Validated @RequestBody CustomerDTO dto){
         Customer customer = customerService.save(convertToEntity(dto));
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(convertToDto(customer));
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(customer.getId()).toUri();
+        log.info("customer saved: {}", customer);
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{id}")
@@ -39,7 +44,7 @@ public class CustomerController {
     }
 
     @PutMapping("/{id}")
-    ResponseEntity<CustomerDTO> update(@PathVariable long id, @RequestBody CustomerDTO dto){
+    ResponseEntity<CustomerDTO> update(@PathVariable long id, @Validated @RequestBody CustomerDTO dto){
         Customer customer = customerService.update(id, convertToEntity(dto));
         return ResponseEntity.ok(convertToDto(customer));
     }
