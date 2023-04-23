7e7d3fa4d90473a85c3c71ab3fad554a2439717d
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-23 13:23:17 -0500

    added the validations annotations

--- src/main/java/com/backfcdev/customersapirest/model/dto/CustomerDTO.java
+++ src/main/java/com/backfcdev/customersapirest/model/dto/CustomerDTO.java
@@ -1,13 +1,26 @@
 package com.backfcdev.customersapirest.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CustomerDTO {
+    @NotBlank
     private String name;
+    @NotBlank
     private String lastname;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private String phone;
+    @NotNull
     private String direction;
 
 }
